fcac7a74dc9c1e10dc1b43d9fff7a98e5914beb8
Naveen Chinnadurai <dev.iamnaveen@gmail.com>
2024-03-24 11:08:52 +0530

    fixed:deployment error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/fcc-drumMachine-reactWebApp/",
   plugins: [react()],
+  base:"/freecodecamp-frontend-dev-libraries/",
 })
